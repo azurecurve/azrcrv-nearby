bdb58f6ea9a48d1b9bbf37a9689e2391fa2f59e2
azurecurve <github@azurecurve.co.uk>
2024-02-03 14:59:11 +0000

    Release v3.0.3

--- azrcrv-nearby.php
+++ azrcrv-nearby.php
@@ -1,14 +1,18 @@
 <?php
 /**
  * ------------------------------------------------------------------------------
- * Plugin Name: Nearby
- * Description: Creates table of nearby locations based on GPS co-ordinates.
- * Version: 3.0.2
- * Author: azurecurve
- * Author URI: https://development.azurecurve.co.uk/classicpress-plugins/
- * Plugin URI: https://development.azurecurve.co.uk/classicpress-plugins/nearby/
- * Text Domain: azrcrv-n
- * Domain Path: /languages
+ * Plugin Name:		Nearby
+ * Description:		Creates table of nearby locations based on GPS co-ordinates.
+ * Version:			3.0.3
+ * Requires CP:		1.0
+ * Author:			azurecurve
+ * Author URI:		https://development.azurecurve.co.uk/classicpress-plugins/
+ * Plugin URI:		https://development.azurecurve.co.uk/classicpress-plugins/nearby/
+ * Donate link:		https://development.azurecurve.co.uk/support-development/
+ * Text Domain:		azrcrv-n
+ * Domain Path:		/languages
+ * License:			GPLv2 or later
+ * License URI:		http://www.gnu.org/licenses/gpl-2.0.html
  * ------------------------------------------------------------------------------
  * This is free software released under the terms of the General Public License,
  * version 2, or later. It is distributed WITHOUT ANY WARRANTY; without even the
